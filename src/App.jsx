6c36ad5e059a6e341fc73772fd20149dfe704f76
jefik <jansilak@seznam.cz>
2026-01-21 18:30:38 +0100

    feat: add animated tiles and onscreen keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import validWords from "./words.json";
+import { motion } from "motion/react";
 
 function compareGuess(guess, target) {
   const result = Array(guess.length).fill("absent");
@@ -60,6 +61,7 @@ async function isValidWord(word) {
     return false;
   }
 }
+
 function App() {
   const [targetWord, setTargetWord] = useState("");
   const [currentGuess, setCurrentGuess] = useState("");
@@ -68,12 +70,33 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [currentRow, setCurrentRow] = useState(0);
+  const [isInvalid, setIsInvalid] = useState(false);
   const WORD_LENGTH = 5;
   const MAX_ATTEMPTS = 6;
+  const Keyboard = [
+    ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+    ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "BACKSPACE"],
+  ];
+  const [keyStatus, setKeyStatus] = useState({});
 
   /* const targetWord = "APPLE"; */
   const [message, setMessage] = useState("");
   const [showTargetWord, setShowTargetWord] = useState(false);
+
+  function updateKeyStatus(guessLetters) {
+    setKeyStatus((prev) => {
+      const newStatus = { ...prev };
+      guessLetters.forEach(({ char, status }) => {
+        // Only upgrade status if correct > present > absent
+        const oldStatus = prev[char];
+        if (oldStatus === "correct") return;
+        if (oldStatus === "present" && status === "absent") return; // Dont downgrade
+        newStatus[char] = status;
+      });
+      return newStatus;
+    });
+  }
 
   useEffect(() => {
     function loadTargetWord() {
@@ -110,6 +133,9 @@ function App() {
         const validWord = await isValidWord(currentGuess);
         if (!validWord) {
           setMessage("Word is not valid");
+          // For Shake animation
+          setIsInvalid(true);
+          setTimeout(() => setIsInvalid(false), 500);
           return;
         } else {
           setMessage("");
@@ -118,6 +144,9 @@ function App() {
         // Check if word was already guessed
         if (guessedWord(currentGuess, guesses)) {
           setMessage("You already guessed that word");
+          // For Shake animation
+          setIsInvalid(true);
+          setTimeout(() => setIsInvalid(false), 500);
           return;
         }
 
@@ -139,6 +168,8 @@ function App() {
             letters: guessletters, // Add the current new guess
           },
         ]);
+
+        updateKeyStatus(guessletters);
 
         // Check if guess word is correct
         if (currentGuess === targetWord) {
@@ -201,54 +232,111 @@ function App() {
 
   return (
     <>
-      <div className="app container">
+      <div className="app">
         <div className="board">
+          <h1>Wordle</h1>
+          <p className="text-danger">{message}</p>
           <p className="text-center">
             Word is: {showTargetWord ? targetWord : "*****"} <br />
             <button className="btn btn-primary" onClick={() => setShowTargetWord(!showTargetWord)}>
               {showTargetWord ? "Hide" : "Reveal"} Word
             </button>
-            <br />
+            {/*  <br />
             Current guess:
             <strong>
               {currentGuess} <br />
               Row: {currentRow + 1} / {MAX_ATTEMPTS} <br /> Status: {gameStatus} <br />
               {message} <br />
-            </strong>
+            </strong> */}
           </p>
 
           {Array.from({ length: MAX_ATTEMPTS }).map((value, rowIndex) => {
             const guess = guesses[rowIndex];
-
+            const isCurrentRow = rowIndex === currentRow;
             return (
-              <div key={rowIndex} className="d-flex justify-content-center mb-2">
-                {Array.from({ length: WORD_LENGTH }).map((value, cellIndex) => {
-                  let char = "";
-                  let status = null;
+              <motion.div
+                key={rowIndex}
+                className="d-flex justify-content-center mb-2"
+                // If its current row and its not valid then shake animation
+                animate={isCurrentRow && isInvalid ? { x: [-10, 10, -10, 10, 0] } : { x: 0 }}
+                transition={{ duration: 0.4 }}
+              >
+                <div key={rowIndex} className="d-flex gap-2 justify-content-center mb-2">
+                  {Array.from({ length: WORD_LENGTH }).map((value, cellIndex) => {
+                    let char = "";
+                    let status = null;
 
-                  // If guess word exists for this row, show it with status
-                  if (guess) {
-                    const cell = guess.letters[cellIndex];
-                    // If cell exists, get char and status
-                    if (cell) {
-                      char = cell.char;
-                      status = cell.status;
+                    // If guess word exists for this row, show it with status
+                    if (guess) {
+                      const cell = guess.letters[cellIndex];
+                      // If cell exists, get char and status
+                      if (cell) {
+                        char = cell.char;
+                        status = cell.status;
+                      }
                     }
-                  }
-                  // If this is active row, show current guess word letters
-                  else if (rowIndex === currentRow) {
-                    char = currentGuess[cellIndex] || "";
-                  }
+                    // If this is active row, show current guess word letters
+                    else if (rowIndex === currentRow) {
+                      char = currentGuess[cellIndex] || "";
+                    }
 
-                  return (
-                    <div key={cellIndex} className={`cell ${status ? `cell--${status}` : ""}`}>
-                      {char}
-                    </div>
-                  );
-                })}
-              </div>
+                    return (
+                      <motion.div
+                        key={cellIndex}
+                        className={`cell ${status ? `cell--${status}` : ""}`}
+                        // Calculation for the animation times,
+                        style={{
+                          transitionDelay: status ? `${cellIndex * 0.15 + 0.25}s` : "0s",
+                        }}
+                        animate={
+                          gameStatus === "won" && rowIndex === currentRow
+                            ? {
+                                y: [0, -30, 0],
+                              }
+                            : status
+                            ? { rotateX: [0, 90, 0] }
+                            : {}
+                        }
+                        transition={
+                          gameStatus === "won" && rowIndex === currentRow
+                            ? {
+                                delay: cellIndex * 0.15,
+                                duration: 0.5,
+                                repeat: 3,
+                                ease: "easeInOut",
+                              }
+                            : {
+                                delay: status ? cellIndex * 0.15 : 0,
+                                duration: 0.5,
+                                times: [0, 0.5, 1],
+                              }
+                        }
+                      >
+                        {char}
+                      </motion.div>
+                    );
+                  })}
+                </div>
+              </motion.div>
             );
           })}
+          <div className="keyboard">
+            {Keyboard.map((row, rowIndex) => (
+              <div key={rowIndex} className="keyboard-row">
+                {row.map((letter) => (
+                  <button
+                    key={letter}
+                    className={`key ${letter === "ENTER" || letter === "BACKSPACE" ? "key--wide" : ""} ${
+                      keyStatus[letter] ? `cell--${keyStatus[letter]}` : ""
+                    }`}
+                    onClick={() => handleKeyClick(letter)}
+                  >
+                    {letter === "BACKSPACE" ? "⌫" : letter}
+                  </button>
+                ))}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
